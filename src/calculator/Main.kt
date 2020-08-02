e2b2a61985efe1811a14702e28910984d057d13b
Michal Czapiewski <czpapieskimk@gmail.com>
2020-08-02 23:10:11 +0200

    feat(Proper valid equation check): Simplified empty input, command and correct expression checks

--- src/calculator/Main.kt
+++ src/calculator/Main.kt
@@ -4,6 +4,10 @@ import java.util.*
 
 val scanner = Scanner(System.`in`)
 val mathSymbols = arrayOf('+', '-')
+const val helpMessage = "The program is a calculator that can return results of basic " +
+        "calculations and keep in memory previous inputs, commands, and results. " +
+        "Supported action: addition, subtraction. " +
+        "Type /exit to exit."
 
 class MathInput {
     private var newLine = arrayOf<String>()
@@ -11,38 +15,40 @@ class MathInput {
     var arrayOfCommands: Array<String?> = arrayOf()
     val isCorrect: Boolean
         get() {
-            return length % 2 != 0 && validateSyntax() && validateEquation()
-        }
-    val exit: Boolean
-        get() {
-            if ("/exit" in arrayOfCommands) {
-                println("Bye!")
-                return true
-            }
+            if (!isCommand && !isEmpty) return validateEquation() && validateSyntax()
             return false
         }
-    val help: Boolean
-        get() {
-            if ("/help" in arrayOfCommands) {
-                println("The program calculates the sum of numbers. Multiple arithmetic symbols " +
-                        "(e.g. -- = +; --- = -, ++ = +) are supported")
-                return true
-            }
-            return false
-        }
-    private val length: Int
-        get() {
-            return arrayOfArguments.size
-        }
+    var isEmpty: Boolean = true
+    var isCommand: Boolean = false
+    var unknownCommand: Boolean = false
+    var exit: Boolean = false
+    var help: Boolean = false
     val lastIndex: Int
         get() {
             return arrayOfArguments.lastIndex
         }
 
+    private fun checkIfCommand() {
+        if (newLine.first()[0] == '/') {
+            isCommand = true
+            when (newLine.first()) {
+                "/exit" -> exit = true
+                "/help" -> help = true
+                else -> unknownCommand = true
+            }
+        }
+    }
+
     fun read() {
-        val inputToList = scanner.nextLine().split(" ")
-        newLine = inputToList.filter { element -> element.isNotEmpty() }.toTypedArray()
-        interpret()
+        val newInput = scanner.nextLine().split(" ")
+        newLine = newInput.filter { element -> element.isNotEmpty() }.toTypedArray()
+        isEmpty = newLine.isEmpty()
+        if (!isEmpty) {
+            checkIfCommand()
+            if (!isCommand) {
+                interpret()
+            }
+        }
     }
 
     private fun interpret() {
@@ -57,15 +63,43 @@ class MathInput {
         }
     }
 
-    private fun repeatedSymbolCheck(elementsIndex: Int): Boolean {
-        val element = arrayOfCommands[elementsIndex]
-        for (i in element!!) {
-            if (i !in mathSymbols || i != element[0]) {
-                return false
+    private fun validateEquation(): Boolean {
+        when {
+            arrayOfArguments.first() == null -> return false
+            arrayOfArguments.last() == null -> return false
+        }
+        for (i in 1 until arrayOfArguments.size) {
+            when {
+                arrayOfArguments[i] != null && arrayOfArguments[i - 1] != null -> return false
+                arrayOfArguments[i] == null && arrayOfArguments[i - 1] == null -> return false
             }
         }
-        if (element.length > 1) convertRepeatedSymbol(elementsIndex)
         return true
+    }
+
+    private fun validateSyntax(): Boolean {
+        for (i in arrayOfCommands.indices) {
+            if (arrayOfCommands[i] != null) {
+                if (!mathSymbolsCheck(i)) return false
+            }
+        }
+        return arrayOfArguments.isNotEmpty()
+    }
+
+    private fun mathSymbolsCheck(elementsIndex: Int): Boolean {
+        val element = arrayOfCommands[elementsIndex]
+        if (element!!.length == 1) {
+            if (element[0] in mathSymbols) return true
+        } else {
+            if (element[0] in mathSymbols) {
+                for (i in 1 until element.length) {
+                    if (element[i] != element[0]) return false
+                }
+                convertRepeatedSymbol(elementsIndex)
+                return true
+            }
+        }
+        return false
     }
 
     private fun convertRepeatedSymbol(elementsIndex: Int) {
@@ -79,32 +113,6 @@ class MathInput {
                 arrayOfCommands[elementsIndex] = "-"
             }
         }
-    }
-
-    private fun validateSyntax(): Boolean {
-        for (i in arrayOfCommands.indices) {
-            if (arrayOfCommands[i] != null) {
-                if (!repeatedSymbolCheck(i)) {
-                    return false
-                }
-            }
-        }
-        return arrayOfArguments.isNotEmpty()
-    }
-
-    private fun validateEquation(): Boolean {
-        for (i in arrayOfArguments.indices) {
-            if (i % 2 == 0) {
-                if (arrayOfArguments[i] == null || arrayOfCommands[i] != null) {
-                    return false
-                }
-            } else {
-                if (arrayOfArguments[i] != null || arrayOfCommands[i] == null) {
-                    return false
-                }
-            }
-        }
-        return true
     }
 }
 
@@ -143,11 +151,25 @@ object Calculator {
     fun nextAction(): Boolean {
         val newInput = MathInput()
         newInput.read()
-        if (newInput.exit) return true
-        if (newInput.help) return false
+        if (newInput.isEmpty) return false
+        if (newInput.isCommand) {
+            when {
+                newInput.exit -> {
+                    println("Bye!")
+                    return true
+                }
+                newInput.help -> {
+                    println(helpMessage)
+                }
+                newInput.unknownCommand -> println("Unknown command")
+            }
+            return false
+        }
         if (newInput.isCorrect) {
             saveInput(newInput)
             println(calculate().toInt())
+        } else {
+            println("Invalid expression")
         }
         return false
     }
